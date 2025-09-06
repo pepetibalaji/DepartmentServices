58e6f702bb4a824a4e9dce20e75e16bc57c6bc75
PEPETI BALAJI <balajipepeti2002@gmail.com>
2025-09-06 19:26:43 +0530

    service registry implementation

--- src/main/java/department/services/ServicesApplication.java
+++ src/main/java/department/services/ServicesApplication.java
@@ -2,11 +2,13 @@ package department.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import jakarta.transaction.Transactional;
 
 @Transactional
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServicesApplication {
 
 	public static void main(String[] args) {
